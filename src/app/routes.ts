111d7b6fa60efa74d36626b8f31287718556b238
Your Name <you@example.com>
2024-04-23 12:44:13 +0300

    add layout

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { Posts } from './widgets/posts/posts';
-import { Form } from './widgets/form/form';
+import { PostCreate } from './widgets/form/form';
 import { Preview } from './widgets/preview/preview';
 
 export const routes: Routes = [
@@ -14,6 +14,6 @@ export const routes: Routes = [
     },
     {
         path: 'write',
-        component: Form,
+        component: PostCreate,
     },
 ];
--- src/app/widgets/form/form.ts
+++ src/app/widgets/form/form.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
  */
 
 @Component({
-    selector: 'form',
+    selector: 'post-create',
     standalone: true,
     imports: [],
     templateUrl: './form.html',
     styleUrl: './styles.css'
 })
-export class Form {
-    title = 'form';
+export class PostCreate {
+    title = 'post-create';
 }
